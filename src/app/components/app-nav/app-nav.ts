69c2e9e41795f3d0da8ae6b77db07d19fdc7cc80
p.janus <p.janus@kontial.com>
2025-11-16 20:00:57 +0100

    small improvments

--- src/app/components/app-nav/app-nav.ts
+++ src/app/components/app-nav/app-nav.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HorizontalSpacing} from '@components/horizontal-spacing/horizontal-spacing';
 import {NavButton} from '@components/nav-button/nav-button';
+import {Logo} from '@components/logo/logo';
 
 @Component({
   selector: 'app-nav',
   imports: [
     HorizontalSpacing,
-    NavButton
+    NavButton,
+    Logo
   ],
   templateUrl: './app-nav.html',
   styleUrl: './app-nav.css'
