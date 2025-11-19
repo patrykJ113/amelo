80ce80389438564186a314764b3a33d43f80950d
p.janus <p.janus@kontial.com>
2025-11-19 23:34:40 +0100

    Implement a error state in input, plus make it bigger

--- src/app/components/app/app-input/app-input.ts
+++ src/app/components/app/app-input/app-input.ts
@@ -1,12 +1,22 @@
 import {Component, Input} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgClass
+  ],
   templateUrl: './app-input.html',
   styleUrl: './app-input.css'
 })
 export class AppInput {
   @Input() label: string = ''
   @Input() disabled: boolean = false
+  @Input() control!: FormControl
+
+  get isError() {
+    return this.control.invalid && ( this.control.touched || this.control.dirty )
+  }
 }
