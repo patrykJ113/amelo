c8472e2dbc9f5e3c50a6d02737b3c162553df540
p.janus <p.janus@kontial.com>
2025-12-01 22:57:26 +0100

    add type attribute to input component

--- src/app/components/app/app-input/app-input.ts
+++ src/app/components/app/app-input/app-input.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {NgClass} from '@angular/common';
+import {InputType} from '@typings/input-type';
 
 @Component({
   selector: 'app-input',
@@ -14,6 +15,7 @@ import {NgClass} from '@angular/common';
 export class AppInput implements OnInit {
   @Input() label: string = ''
   @Input() disabled: boolean = false
+  @Input() type: InputType = 'text'
   @Input() isTextArea: boolean = false
   @Input() control!: FormControl
 
