094fa5acfa75d6f90bcdc4d3f625c11d8b97e3e1
p.janus <p.janus@kontial.com>
2025-12-08 12:16:30 +0100

    save

--- src/app/pages/add-listing-page/add-listing-page.ts
+++ src/app/pages/add-listing-page/add-listing-page.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AppButton} from '@components/app/app-button/app-button';
+import {ListingForm} from '@components/listing-form/listing-form';
+import {SvgIconComponent} from 'angular-svg-icon';
+import {VerticalSpacing} from '@components/positioning/vertical-spacing/vertical-spacing';
+import {HorizontalSpacing} from '@components/positioning/horizontal-spacing/horizontal-spacing';
 
 @Component({
   selector: 'add-listing-page',
-  imports: [],
+  imports: [
+    AppButton,
+    ListingForm,
+    SvgIconComponent,
+    VerticalSpacing,
+    HorizontalSpacing
+  ],
   templateUrl: './add-listing-page.html',
   styleUrl: './add-listing-page.css'
 })
 export class AddListingPage {
-
 }
