bde5ce390dfb9941cb57892118ed97a68008e886
p.janus <p.janus@kontial.com>
2025-12-16 17:37:06 +0100

    add a panel component

---
+++ src/app/components/panel/panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'panel',
+  imports: [],
+  templateUrl: './panel.html',
+  styleUrl: './panel.css'
+})
+export class Panel {
+
+}
