c7aabbaeaeb3c58b006bd39aea049ec09a369015
p.janus <p.janus@kontial.com>
2025-12-16 20:41:53 +0100

    make the image slots focusable

--- src/app/components/image-slot/image-slot.ts
+++ src/app/components/image-slot/image-slot.ts
@@ -1,4 +1,10 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import {SvgIconComponent} from 'angular-svg-icon';
 import {NgClass} from '@angular/common';
 import {FormControl} from '@angular/forms';
@@ -12,14 +18,15 @@ import {FormControl} from '@angular/forms';
   templateUrl: './image-slot.html',
   styleUrl: './image-slot.css'
 })
-export class ImageSlot implements OnInit{
+export class ImageSlot implements OnInit {
   @Input() index!: number
   @Input() control!: FormControl
+  @Output() openFIlePicker: EventEmitter<void> = new EventEmitter
   blobUrl: string = ''
 
   ngOnInit() {
     this.control.valueChanges.subscribe(value => {
-      if(!this.control.value || !this.control.value[this.index]) {
+      if (!this.control.value || !this.control.value[this.index]) {
         this.blobUrl = ''
       } else {
         this.blobUrl = URL.createObjectURL(this.control.value[this.index])
@@ -29,12 +36,21 @@ export class ImageSlot implements OnInit{
 
   get isFirstWithoutImag() {
     if (!this.control.value) return this.index === 0
-    return this.control.value[this.index] === undefined && this.control.value[this.index -1]
+    return this.control.value[this.index] === undefined && this.control.value[this.index - 1]
   }
 
   removeImage() {
-    const arr = [...this.control.value];
-    arr.splice(this.index, 1);
-    this.control.setValue(arr);
+    const tmpArr = [...this.control.value];
+    tmpArr.splice(this.index, 1);
+
+    let controlValue = tmpArr.length ? tmpArr : null
+    this.control.setValue(controlValue);
+  }
+
+  onKayDown({key}: KeyboardEvent) {
+    const isEnter = key === 'Enter'
+    if (isEnter) {
+      this.blobUrl ? this.removeImage() : this.openFIlePicker.emit()
+    }
   }
 }
