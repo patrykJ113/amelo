23aff58880b370c52a9a40cca36dba70b8eebe40
p.janus <p.janus@kontial.com>
2025-12-19 16:50:11 +0100

    add a base listing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {MainLayout} from '@layouts/main-layout/main-layout';
 import {AddListingPage} from '@pages/add-listing-page/add-listing-page';
 import {ListingsPage} from '@pages/listings-page/listings-page';
+import {ListingPage} from '@pages/listing-page/listing-page';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,10 @@ export const routes: Routes = [
           {
             path: 'add',
             component: AddListingPage
+          },
+          {
+            path: ':id',
+            component: ListingPage
           }
         ]
       },
