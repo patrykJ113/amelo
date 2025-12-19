2c380b614b5132c3523b028e5ef16e8eb6c776c1
p.janus <p.janus@kontial.com>
2025-12-19 16:50:01 +0100

    add a base listing page

---
+++ src/app/pages/listing-page/listing-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListingPage } from './listing-page';
+
+describe('ListingPage', () => {
+  let component: ListingPage;
+  let fixture: ComponentFixture<ListingPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListingPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListingPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/listing-page/listing-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'listing-page',
+  imports: [],
+  templateUrl: './listing-page.html',
+  styleUrl: './listing-page.css'
+})
+export class ListingPage {
+
+}
