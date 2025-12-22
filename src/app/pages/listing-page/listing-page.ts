9c29e4847cf9c3511385ab64319ef4d2dbcc333a
p.janus <p.janus@kontial.com>
2025-12-22 13:07:46 +0100

    small restyle plus fetch pictures

--- src/app/pages/listing-page/listing-page.ts
+++ src/app/pages/listing-page/listing-page.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ListingService} from '@services/listing.service';
+import {ActivatedRoute} from '@angular/router';
+import {Listing} from '@typings/listing';
+import {Picture} from '@typings/picture';
+import {forkJoin} from 'rxjs';
+import {Panel} from '@components/panel/panel';
+import {VerticalSpacing} from '@components/positioning/vertical-spacing/vertical-spacing';
 
 @Component({
   selector: 'listing-page',
-  imports: [],
+  imports: [
+    Panel,
+    VerticalSpacing
+  ],
   templateUrl: './listing-page.html',
   styleUrl: './listing-page.css'
 })
-export class ListingPage {
+export class ListingPage implements OnInit {
+  listingId: string | null = null
+  listing: Listing | undefined = undefined
+  pictureUrls: String[] = []
+
+  constructor(
+    private listingService: ListingService,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit() {
+    this.listingId = this.route.snapshot.paramMap.get('id')
+
+    if (this.listingId) this.loadListing(this.listingId)
+  }
+
+  loadListing(listingId: string) {
+    this.listingService.getById(listingId).subscribe({
+      next: listing => {
+        this.listing = listing
+        this.loadListingPictures(this.listing.pictures)
+        console.log(listing)
+      },
+      error: err => {
+        console.error(err)
+      }
+    })
+  }
+
+  loadListingPictures(pictures: Picture[]) {
+    const requests = pictures.map(pic => this.listingService.getImage(this.listing!.id, pic.file_name))
+
+    forkJoin(requests).subscribe(blobs => {
+      this.pictureUrls = blobs.map(blob => URL.createObjectURL(blob))
+    })
+  }
 
 }
