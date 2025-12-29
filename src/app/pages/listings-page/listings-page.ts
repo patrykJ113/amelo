18acbb44a2b8018c801685e7d8685b766ac1a61f
Patryk Janus <93097841+patrykJ113@users.noreply.github.com>
2025-12-29 16:05:40 +0100

    add loader in the listings page (#2)
    Co-authored-by: p.janus <p.janus@kontial.com>

--- src/app/pages/listings-page/listings-page.ts
+++ src/app/pages/listings-page/listings-page.ts
@@ -3,18 +3,21 @@ import {ListingCard} from '@components/listing-card/listing-card';
 import {ListingService} from '@services/listing.service';
 import {GetAllListingsResponse} from '@typings/listing';
 import {RouterLink} from '@angular/router';
+import {Loading} from '@components/loading/loading';
 
 @Component({
   selector: 'listings-page',
   imports: [
     ListingCard,
-    RouterLink
+    RouterLink,
+    Loading
   ],
   templateUrl: './listings-page.html',
   styleUrl: './listings-page.css'
 })
 export class ListingsPage implements OnInit {
   listings: GetAllListingsResponse = []
+  loading: boolean = false
   constructor(private listingService: ListingService) {
   }
 
@@ -23,13 +26,26 @@ export class ListingsPage implements OnInit {
   }
 
   getListings() {
+    this.showLoader()
     this.listingService.getAll().subscribe({
       next: listings => {
         this.listings = listings
+        this.hideLoader()
       },
       error: err => {
         console.log(err)
+        this.hideLoader()
       }
     })
+  }
+
+  showLoader() {
+    this.loading = true
+  }
+
+  hideLoader() {
+    setTimeout(() => {
+      this.loading = false
+    }, 3000)
   }
 }
