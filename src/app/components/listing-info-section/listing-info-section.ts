5dd4b0ed64f3e7a6f1829ce9da23c25103ab84a2
Patryk Janus <93097841+patrykJ113@users.noreply.github.com>
2026-01-02 19:25:38 +0100

    use proper formating for price on listing page (#11)
    Co-authored-by: p.janus <p.janus@kontial.com>

--- src/app/components/listing-info-section/listing-info-section.ts
+++ src/app/components/listing-info-section/listing-info-section.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Listing} from '@typings/listing';
 import {Panel} from '@components/panel/panel';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'listing-info-section',
   imports: [
-    Panel
+    Panel,
+    CurrencyPipe
   ],
   templateUrl: './listing-info-section.html',
   styleUrl: './listing-info-section.css'
