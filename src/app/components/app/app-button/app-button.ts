31d76abdf96ab289528dec11e4eb81f5c3f9fce5
Patryk Janus <93097841+patrykJ113@users.noreply.github.com>
2026-01-26 22:26:44 +0100

    [AM-72] - As a user i want states for the button (#14)
    * add variants to button component
    
    * add variants to button component
    
    ---------
    
    Co-authored-by: p.janus <p.janus@kontial.com>

--- src/app/components/app/app-button/app-button.ts
+++ src/app/components/app/app-button/app-button.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {NgClass} from '@angular/common';
+import {ButtonVariant} from '@typings/button-variant';
 
 @Component({
   selector: 'app-button',
@@ -13,4 +14,53 @@ export class AppButton {
   @Input() label: string = 'Click'
   @Input() disabled: boolean = false
   @Input() type: 'button' | 'submit' = 'button'
+  @Input() variant: ButtonVariant = 'Primary'
+
+  get isPrimary() {
+    return this.variant === 'Primary' && this.isEnabled
+  }
+
+  get isOutline() {
+    return this.variant === 'Outline' && this.isEnabled
+  }
+
+  get isText() {
+    return this.variant === 'Text' && this.isEnabled
+  }
+
+  get showRingOnFocusVisible() {
+    return this.isPrimary || this.isOutline
+  }
+
+  get hasCursorPointer() {
+    return this.allBesideDisabled
+  }
+
+  get blueTextAndBlueBgOnActive() {
+    return this.isOutline || this.isText
+  }
+
+  get hasTransparentBorder() {
+    return this.isText
+  }
+
+  get scaleTextOnHover() {
+    return this.allBesideDisabled
+  }
+
+  get allBesideDisabled() {
+    return this.isPrimary || this.isOutline || this.isText
+  }
+
+  get isDisabledPrimary() {
+    return this.variant === 'Primary' && this.disabled
+  }
+
+  get isDisabledText() {
+    return this.variant === 'Text' && this.disabled
+  }
+
+  get isEnabled() {
+    return !this.disabled
+  }
 }
