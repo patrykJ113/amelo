873fff201a6d243a72ebc9e18985e7047a6f23e6
p.janus <p.janus@kontial.com>
2026-02-09 19:53:05 +0100

    BUG: fix wierd loking first elemnt in dropown on focus

--- src/app/components/drop-down-menu/drop-down-menu.ts
+++ src/app/components/drop-down-menu/drop-down-menu.ts
@@ -1,4 +1,4 @@
-import {Component, Input, WritableSignal} from '@angular/core';
+import {Component, effect, Input, WritableSignal} from '@angular/core';
 import {NgClass} from '@angular/common';
 import {CdkTrapFocus} from '@angular/cdk/a11y';
 
@@ -9,13 +9,23 @@ import {CdkTrapFocus} from '@angular/cdk/a11y';
     CdkTrapFocus
   ],
   templateUrl: './drop-down-menu.html',
+  standalone: true,
   styleUrl: './drop-down-menu.css'
 })
 export class DropDownMenu {
+  panelTabIndex: number = 0
   @Input() open: WritableSignal<boolean> | undefined
 
   onEscKeyDown() {
     this.open?.set(false)
+  }
+
+  constructor() {
+    effect(() => {
+      if(this.open?.()) {
+        this.panelTabIndex = 0
+      }
+    });
   }
 
   close() {
@@ -24,5 +34,11 @@ export class DropDownMenu {
 
   get isOpen(): boolean {
     return this.open ? this.open() : false
+  }
+
+  onFocusIn() {
+    if(this.panelTabIndex === 0) {
+      this.panelTabIndex = -1
+    }
   }
 }
