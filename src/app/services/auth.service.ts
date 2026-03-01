6311f77e1cec84bb7d03b0f83476d703014e642c
p.janus <p.janus@kontial.com>
2026-03-01 20:16:00 +0100

    implement new auth methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,60 @@
 import { Injectable } from '@angular/core';
 import {environment} from '@environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Credentials} from '@typings/Credentials';
 import {Observable} from 'rxjs';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private authApiUrl: string = `${environment.apiUrl}/auth`
+  private token: string
 
   constructor(private http: HttpClient) {
+    const tokenInLs = localStorage.getItem(this.localStorageJwtKey)
+    this.token = tokenInLs ? tokenInLs : ''
   }
 
-  logIn(credentials: Credentials): Observable<void> {
-    return this.http.post<void>(`${this.authApiUrl}/login`, credentials)
+  setToken(jwt: string) {
+    localStorage.setItem(this.localStorageJwtKey, jwt)
+  }
+
+  getToken() {
+    return localStorage.getItem(this.localStorageJwtKey)
+  }
+
+  isLoggedIn() {
+    const token = this.getToken()
+
+    if(token) {
+      const decoded = jwtDecode(token)
+      if(!decoded.exp) return false
+      const expiresAt = decoded.exp;
+      const now = Date.now()
+      return expiresAt < now
+    }
+    return false
+  }
+
+  private unsetToken() {
+    localStorage.removeItem(this.localStorageJwtKey)
+  }
+
+  private get localStorageJwtKey() {
+    return 'jwt'
+  }
+
+  logIn(credentials: Credentials): Observable<HttpResponse<void>> {
+    return this.http.post<void>(`${this.authApiUrl}/login`, credentials, { observe: 'response' })
   }
 
   register(credentials: Credentials) {
     return this.http.post<string>(`${this.authApiUrl}/register`, credentials)
   }
 
-  logOut() {}
+  logOut() {
+    this.unsetToken()
+  }
 }
