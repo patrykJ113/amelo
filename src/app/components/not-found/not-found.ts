17bc68dee28cb23143aad3a4a0b8ad070ef741fd
p.janus <p.janus@kontial.com>
2026-04-09 12:30:43 +0200

    remove unused import

--- src/app/components/not-found/not-found.ts
+++ src/app/components/not-found/not-found.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import {SvgIconComponent} from 'angular-svg-icon';
-import {VerticalSpacing} from '@components/positioning/vertical-spacing/vertical-spacing';
 import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'not-found',
   imports: [
     SvgIconComponent,
-    VerticalSpacing,
     RouterLink
   ],
   templateUrl: './not-found.html',
